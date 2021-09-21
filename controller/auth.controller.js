24ef6a931cfe16214df3830e6a48f7c2d48055df
emmanz95 <emmanza2@gmail.com>
2021-09-21 22:22:49 +0100

    wrote the login function

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const User = mongoose.model('user');
+const { JWT_SECRET } = require('../key');
 
 const register = (req, res) => {
     const { nickname, fullName, email, password, dob, country } = req.body;
@@ -15,14 +16,7 @@ const register = (req, res) => {
             }
             bcrypt.hash(password, 14)
                 .then((bcryptPassword) => {
-                    const user = new User({
-                        nickname,
-                        fullName,
-                        email,
-                        password: bcryptPassword,
-                        dob,
-                        country
-                    })
+                    const user = new User({ nickname, fullName, email, password: bcryptPassword, dob, country })
                     user.save()
                         .then((savedUser) => {
                             return res.status(201).json({ savedUser })
@@ -34,6 +28,30 @@ const register = (req, res) => {
         .catch((error) => console.log(error))
 }
 
-const login = (req, res) => {}
+const login = (req, res) => {
+    const { email, password } = req.body;
+    if(!email || !password) {
+        return res.status(200).json({ message: 'Email or password input empty!' })
+    }
+    User.findOne({ email: email })
+        .then(savedUser => {
+            if(!savedUser) {
+                res.json({ message: 'User email not found!' })
+            }
+            bcrypt.hash(password, savedUser.password)
+                .then(doMatch => {
+                    if(doMatch) {
+                        const token = jwt.sign({ _id: savedUser._id }, JWT_SECRET, { expiresIn: '2d' })
+                        const { nickname, fullName, email, dob, country } = savedUser;
+                        res.json({ token, user: { nickname, fullName, email, dob, country }})
+                    } else {
+                        return res.json({ message: 'Invalid email or password' })
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        })
+}
 
 module.exports = {register, login}
