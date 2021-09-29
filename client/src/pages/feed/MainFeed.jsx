adf65f9eeb60057c37edce70dbdf84c6e9a745c3
Emmanuel Okuchukwu <emmanza2@gmail.com>
2021-09-29 14:35:27 +0100

    new change

--- client/src/pages/feed/MainFeed.jsx
+++ client/src/pages/feed/MainFeed.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { AuthService } from '../../service/authService';
+import { MainFeedSection } from './mainFeedStyles';
 
 function MainFeed() {
     const [userInfo, setUserInfo] = useState(AuthService.getUserInfo());
     return(
-        <div></div>
+        <MainFeedSection></MainFeedSection>
     )
 }
 
